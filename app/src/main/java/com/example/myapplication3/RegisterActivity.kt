a32fcdb1b4d9391ee568279cec682823b63902e2
Mate27 <mate.antadze.1@btu.edu.ge>
2022-01-12 17:07:47 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication3/RegisterActivity.kt
@@ -0,0 +1,69 @@
+package com.example.myapplication3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var editTextPassword2: EditText
+    private lateinit var buttonRegister: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        init()
+        registerListeners()
+
+    }
+
+    private fun init() {
+
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        editTextPassword2 = findViewById(R.id.editTextPassword2)
+        buttonRegister = findViewById(R.id.buttonRegister)
+
+    }
+
+    private fun registerListeners() {
+        buttonRegister.setOnClickListener() {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        buttonRegister.setOnClickListener(){
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            val password2 = editTextPassword2.text.toString()
+
+
+            if(email.isEmpty() || password.isEmpty() || password2.isEmpty()) {
+
+                Toast.makeText(this, "Empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+
+                    }else{
+                        Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+    }
+
+
+
+}
